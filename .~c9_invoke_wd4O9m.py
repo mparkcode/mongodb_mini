f1c0f815d168cf54de8fc7f887929a0eced70871
Des Flaherty <desflaherty@hotmail.com>
2019-03-13 15:38:34 +0000

    add task

--- .~c9_invoke_wd4O9m.py
+++ .~c9_invoke_wd4O9m.py
@@ -1,30 +1,27 @@
-import os
-from flask import Flask , render_template,redirect,request,url_for
+mport os
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:Allergan99@myfirstcluster-lgqe5.mongodb.net/task_manager?retryWrites=true'
-
+app.config["MONGO_DBNAME"] = 'task_manager'
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
-    
-    
+
+
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
-            
-            
