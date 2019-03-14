12055fde7634fa99a5519571ee78b810c80b39fe
Des Flaherty <desflaherty@hotmail.com>
2019-03-14 15:07:54 +0000

    add categories

--- app.py
+++ app.py
@@ -55,7 +55,11 @@ def update_task(task_id):
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
-
+    
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
 
 
 
